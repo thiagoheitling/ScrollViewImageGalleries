91c29351153cb59370d3d1a9e2a13eee67f4daf7
Thiago Heitling <thiago.heitling@gmail.com>
2016-01-25 16:51:31 -0800

    first commit

---
+++ ScrollViewImageGalleries/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ScrollViewImageGalleries
+//
+//  Created by Thiago Heitling on 2016-01-25.
+//  Copyright © 2016 Thiago Heitling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *viewScrolled;
+
+@end
+
---
+++ ScrollViewImageGalleries/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  ScrollViewImageGalleries
+//
+//  Created by Thiago Heitling on 2016-01-25.
+//  Copyright © 2016 Thiago Heitling. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIImageView *image1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-in-Field"]];
+    UIImageView *image2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-night"]];
+    UIImageView *image3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-zoomed"]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
